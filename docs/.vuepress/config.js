5498ba7e3aee8a393234bcabe310bd4d9e9eed79
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-05-03 22:23:46 -0500

    update url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  base: '/ream-doc/',
+  base: '/ream-doc-v2/',
   title: 'REAM',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
